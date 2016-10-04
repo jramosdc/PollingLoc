cfe3368183d584dc019df5cc93b4ebf41f902009
jramosdc <mail.jairo@gmail.com>
2016-10-04 06:17:20 -0700

    adding gelocation in app.py

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import urllib2
 import json, requests
 
 # Automatically geolocate the connecting IP
-##f = urllib2.urlopen('http://freegeoip.net/json/')
-##json_string = f.read()
-##f.close()
-##location = json.loads(json_string)
-##coord= "{},{}".format(location['latitude'],location['longitude'])
+f = urllib2.urlopen('http://freegeoip.net/json/')
+json_string = f.read()
+f.close()
+location = json.loads(json_string)
+coord= "{},{}".format(location['latitude'],location['longitude'])
 
 url = 'https://www.googleapis.com/civicinfo/v2/voterinfo'
 
 params = dict(
-    address='72+Frambes+Ave,+Columbus,+OH+43201',
+    address= coord,
     electionId='2000',
     fields='pollingLocations',
     key='AIzaSyAzNEkhYYRuLAgVU2ghn1LY9ulanWBd1x0'
