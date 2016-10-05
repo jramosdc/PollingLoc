73e6ecdde945c91861b1e05fda5585cdd88d30f0
jramosdc <mail.jairo@gmail.com>
2016-10-05 15:39:32 -0700

    googlemaps

--- vote.py
+++ vote.py
@@ -3,6 +3,8 @@ from flask import request
 from flask import render_template
 import urllib2
 import json, requests
+from googlemaps import GoogleMaps
+
 
 app = Flask(__name__)
 
@@ -14,6 +16,7 @@ def my_form():
 def my_form_post():
 
     coord = request.form['coordinates']
+    gmaps = GoogleMaps('AIzaSyDOglPws4TcoEFJ8O4gTYl9Pst-KFjsD8E')
     url = 'https://www.googleapis.com/civicinfo/v2/voterinfo'
     params = dict(address= coord, electionId='2000',fields='dropOffLocations,earlyVoteSites,normalizedInput,pollingLocations',key='AIzaSyAzNEkhYYRuLAgVU2ghn1LY9ulanWBd1x0')
     resp = requests.get(url=url, params=params)
@@ -23,6 +26,7 @@ def my_form_post():
     line3= data['pollingLocations'][0]['address']['locationName']
     line4= data['pollingLocations'][0]['address']['state']
     line5= data['pollingLocations'][0]['pollingHours']
+    line5= lat, lng = gmaps.address_to_latlng(coord)
     return render_template('result.html', line1=line1,line2=line2,line3=line3,line4=line4,line5=line5)
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
