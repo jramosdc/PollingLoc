13566dda1b365972ccfc6f53b2ab30b047018824
jramosdc <mail.jairo@gmail.com>
2016-10-08 15:40:10 -0700

    put it in form

--- static/geo.js
+++ static/geo.js
@@ -61,7 +61,7 @@ function getLocation(){
   **/
   function outputResult(msg){
     $('.result').addClass('result').html(msg);
-    $('#where').val(msg);
+    $('#where').val(msg).submit();
   }
 } // end getLocation()
 
